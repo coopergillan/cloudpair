20ad33ef10b20e66d11417da1d88b18e60bf775f
Cooper Gillan <cooper.gillan@gmail.com>
2021-03-01 21:30:26 -0600

    GH-1 - Add more helper scripts and fix a bug

--- script/common.sh
+++ script/common.sh
@@ -1,1 +1,1 @@
-export EC2_INSTANCE=$(aws ec2 describe-instances | jq '.Reservations[0].Instances[0].InstanceId')
+export EC2_INSTANCE_ID=$(aws ec2 describe-instances | jq -r '.Reservations[0].Instances[0].InstanceId')
